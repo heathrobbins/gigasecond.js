e38e57976fbb0de55227e52dddf945f84157c7fa
Heath Robbins <heathrobbins89@gmail.com>
2023-06-02 12:47:30 -0400

    Create gigasecond.js

---
+++ gigasecond.js
@@ -0,0 +1,8 @@
+function countSeconds(name){
+      var count = 0; 
+      do {
+      count += 1;
+      console.log(name + " has lived for " + count + " seconds");
+      } while (count<1000000000);
+}
+countSeconds("Heath");
